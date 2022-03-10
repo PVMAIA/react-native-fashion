2d0003b8f8a8ebdee282eb6071af356ba636e670
PVMAIA <pvpaulo16@gmail.com>
2022-03-10 19:24:50 -0300

    Created slideshow

---
+++ src/Authentication/Onboarding/Onboarding.tsx
@@ -0,0 +1,145 @@
+import React, { useRef } from "react";
+import { Dimensions, StyleSheet, View } from "react-native";
+import Animated, {
+  interpolateColor,
+  multiply,
+  useAnimatedScrollHandler,
+  useAnimatedStyle,
+  useDerivedValue,
+  useSharedValue,
+} from "react-native-reanimated";
+
+import Slide, { SLIDE_HEIGHT } from "./Slide";
+import Subslide from "./Subslide";
+
+const BORDER_RADIUS = 75;
+const { width } = Dimensions.get("window");
+const slides = [
+  {
+    title: "Relaxed",
+    subtitle: "Find Your Outfits",
+    description:
+      "Confused about your outfit? Don’t worry! Find the best outfit here!",
+    color: "#BFEAF5",
+  },
+  {
+    title: "Playful",
+    subtitle: "Hear it First, Wear it First",
+    description:
+      "Hating the clothes in your wardrobe? Explore hundreds of outfit ideas",
+    color: "#BEECC4",
+  },
+  {
+    title: "Excentric",
+    subtitle: "Your Style, Your Way",
+    description:
+      "Create your individual & unique style and look amazing everyday",
+    color: "#FFE4D9",
+  },
+  {
+    title: "Funky",
+    subtitle: "Look Good, Feel Good",
+    description:
+      "Discover the latest trends in fashion and explore your personality",
+    color: "#FFDDDD",
+  },
+];
+
+const Onboarding = () => {
+  const scroll = useRef<Animated.ScrollView>(null);
+  const x = useSharedValue(0);
+  const onScroll = useAnimatedScrollHandler({
+    onScroll: ({ contentOffset }) => {
+      x.value = contentOffset.x;
+    },
+  });
+
+  const backgroundColors = useDerivedValue(() =>
+    interpolateColor(
+      x.value,
+      slides.map((_, i) => i * width),
+      slides.map((slide) => slide.color)
+    )
+  );
+
+  const backgroundColor = useAnimatedStyle(() => ({
+    backgroundColor: backgroundColors.value,
+  }));
+
+  const footerContentAnimatedStyle = useAnimatedStyle(() => ({
+    transform: [{ translateX: -x.value }],
+  }));
+
+  return (
+    <View style={styles.container}>
+      <Animated.View style={[styles.slider, backgroundColor]}>
+        <Animated.ScrollView
+          ref={scroll}
+          horizontal
+          snapToInterval={width}
+          decelerationRate="fast"
+          showsHorizontalScrollIndicator={false}
+          bounces={false}
+          scrollEventThrottle={1}
+          onScroll={onScroll}
+        >
+          {slides.map(({ title }, index) => (
+            <Slide key={index} right={!!(index % 2)} {...{ title }} />
+          ))}
+        </Animated.ScrollView>
+      </Animated.View>
+      <View style={styles.footer}>
+        <Animated.View
+          style={[{ ...StyleSheet.absoluteFillObject }, backgroundColor]}
+        />
+        <Animated.View
+          style={[
+            styles.footerContent,
+            {
+              width: width * slides.length,
+            },
+            footerContentAnimatedStyle,
+          ]}
+        >
+          {slides.map(({ subtitle, description }, index) => (
+            <Subslide
+              key={index}
+              last={index === slides.length - 1}
+              onPress={() => {
+                if (scroll.current) {
+                  scroll.current.scrollTo({
+                    x: width * (index + 1),
+                    animated: true,
+                  });
+                }
+              }}
+              {...{ subtitle, description }}
+            />
+          ))}
+        </Animated.View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  slider: {
+    height: SLIDE_HEIGHT,
+    borderBottomRightRadius: BORDER_RADIUS,
+  },
+  footer: {
+    flex: 1,
+  },
+  footerContent: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "white",
+    borderTopLeftRadius: BORDER_RADIUS,
+  },
+});
+
+export default Onboarding;
